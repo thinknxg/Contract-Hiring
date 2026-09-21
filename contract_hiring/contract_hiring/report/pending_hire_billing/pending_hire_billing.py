931d1c96f435f603cbe7eaa401cf44659d32f1b6
RithwikPK <teraboxneed@gmail.com>
2026-09-21 12:17:39 +0530

    Step 3: remove Hire Order / Contract Hiring Quotation, repoint fetch_from, reports, workspace

--- contract_hiring/contract_hiring/report/pending_hire_billing/pending_hire_billing.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute(filters=None):
-    columns = [{"label": "Name", "fieldname": "name", "fieldtype": "Link", "options": "Hire Order", "width": 140}, {"label": "Customer", "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 140}, {"label": "Project", "fieldname": "project", "fieldtype": "Link", "options": "Project", "width": 140}, {"label": "Site", "fieldname": "site", "fieldtype": "Link", "options": "Project Site", "width": 140}, {"label": "Billing Start Date", "fieldname": "billing_start_date", "fieldtype": "Date", "options": "", "width": 140}, {"label": "Billing End Date", "fieldname": "billing_end_date", "fieldtype": "Date", "options": "", "width": 140}, {"label": "Status", "fieldname": "status", "fieldtype": "Data", "options": "", "width": 140}]
-    filters = filters or {}
-    data = frappe.get_all("Hire Order", filters=filters, fields=["name", "customer", "project", "site", "billing_start_date", "billing_end_date", "status"], order_by="modified desc", limit_page_length=500)
-    return columns, data
--- contract_hiring/public/js/customer.js
+++ contract_hiring/public/js/customer.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on("Customer", {
     refresh(frm) {
-        if (!frm.is_new()) frm.add_custom_button("Contract Hiring Quotations",()=>frappe.set_route("List","Contract Hiring Quotation",{customer:frm.doc.name}),"Contract Hiring");
+        if (!frm.is_new()) frm.add_custom_button("Hiring Sales Orders",()=>frappe.set_route("List","Sales Order",{customer:frm.doc.name,hiring_type:["!=",""]}),"Contract Hiring");
     }
 });
